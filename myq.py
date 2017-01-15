d779b1eec128411cff7d91e9d51160ba8285f826
jschwalbe <justin-github@finishtherace.net>
2017-01-14 22:50:19 -0500

    Update myq.py
    The link at the top is no longer valid.. so I changed it to the forum link..

--- myq.py
+++ myq.py
@@ -2,7 +2,7 @@
 Support for MyQ garage doors.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/cover.myq/
+https://community.home-assistant.io/t/myq-componenet-issues/1860/81
 """
 
 import logging
